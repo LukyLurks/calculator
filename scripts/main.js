a43204d7464fc9c8c0d0892b12f6aa4678691c37
Luckner Cyné <luckner@cyne.fr>
2019-06-19 02:46:07 +0200

    Find next operation in an expression with PEMDAS

--- scripts/main.js
+++ scripts/main.js
@@ -10,16 +10,14 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 const pow = (x, n) => x ** n;
 const percent = (a, b) => b / 100 * a;
-const root = x => Math.sqrt(x);
 
 const operate = (operand1, operand2, operator) => {
   if (operator === '+') return add(operand1, operand2);
   else if (operator === '-') return subtract(operand1, operand2);
-  else if (operator === '*') return multiply(operand1, operand2);
-  else if (operator === '/') return divide(operand1, operand2);
+  else if (operator === '×') return multiply(operand1, operand2);
+  else if (operator === '÷') return divide(operand1, operand2);
   else if (operator === '^') return pow(operand1, operand2);
   else if (operator === '%') return percent(operand1, operand2);
-  else if (operator === '√') return root(operand1);
   else return 0;
 };
 
@@ -27,9 +25,42 @@ const parseSimpleExpr = operation => {
   return [undefined, undefined, undefined];
 };
 
-// Takes an expression with parentheses
 const getPriorityOp = expr => {
-  return expr;
+  let hasParentheses = expr.indexOf(')') !== -1;
+  let startIndex = 0;
+  let endIndex = 0;
+
+  if (hasParentheses) {
+    endIndex = expr.indexOf(')') + 1;
+    startIndex = expr.slice(0, endIndex).lastIndexOf('(');
+  } else {
+    let opIndex = 0;
+    let leftOffset = 0;
+    let rightOffset = 0;
+    if (expr.indexOf('^') !== -1) {
+      opIndex = expr.indexOf('^');
+    } else if (expr.indexOf('×') !== -1) {
+      opIndex = expr.indexOf('×');
+    } else if (expr.indexOf('÷') !== -1) {
+      opIndex = expr.indexOf('÷');
+    } else if (expr.indexOf('+') !== -1) {
+      opIndex = expr.indexOf('+');
+    } else if (expr.indexOf('-') !== -1) {
+      opIndex = expr.indexOf('-');
+    }
+
+    leftOffset = expr.slice(0, opIndex)
+                     .split().reverse().join()
+                     .search(/[^0-9]/);
+    if (leftOffset < 0) leftOffset = expr.slice(0, opIndex).length;
+    startIndex = opIndex - leftOffset;
+
+    rightOffset = expr.slice(opIndex + 1).search(/[^0-9]/);
+    if (rightOffset < 0) rightOffset = expr.length - 1;
+    endIndex = opIndex + rightOffset;
+  }
+  
+  return expr.slice(startIndex, endIndex);
 };
 
 const calculateExpression = expr => {
@@ -60,6 +91,9 @@ const updateExpr = (expr, button) => {
     result.textContent = 0;
   } else if (button.id === 'backspace') {
     expr.textContent = expr.textContent.slice(0, -1);
+    if (expr.textContent === '') {
+      expr.textContent = '.';
+    }
   } else if (button.id === 'equals') {
     if (!hasGoodParentheses(expr.textContent)) {
       result.textContent = 'Error (parentheses)';
